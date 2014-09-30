992e1b079fb14ebd7656a28daffee68567fc8da8
hsctch <dacuna@hsctch.com>
2014-09-30 17:48:24 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>.:: UNIGAS | Universal Gas ::.</title>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
+<link href="jquery.mCustomScrollbar.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="images/icon.png" type="image/x-icon" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js" type="text/javascript"></script>
+<script src="scripts/jquery.easing.1.3.js" type="text/javascript"></script>
+<script src="scripts/jquery.mousewheel.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="scripts/pop-ups.js"></script>
+<script type="text/javascript" src="scripts/nav.js"></script>
+</head>
+<body>
+<hgroup>
+  <img src="images/unigas.png" width="300" height="150" >
+  <h4><i>"Lo nuestro siempre primero"</i></h4>
+</hgroup>
+<nav>
+  <ul id="menu" class="menu">
+    <li> <a href="unigas_frontend/index.php"> <img src="images/1_menu.png" alt=""/> <span class="active"></span> <span class="wrap"> <span class="link">Nuestra Empresa</span></span> </a> </li>
+    <li> <a href="#"> <img src="images/2_menu.png" alt=""/> <span class="active"></span> <span class="wrap"> <span class="link">Productos y Servicios</span></span> </a> </li>
+    <li> <a href="#"> <img src="images/3_menu.png" alt=""/> <span class="active"></span> <span class="wrap"> <span class="link">Unigas Contigo</span></span> </a> </li>
+    <li > <a href="#"> <img src="images/2_menu.png" alt=""/> <span class="active"></span> <span class="wrap"> <span class="link">Contácto</span></span> </a> </li>
+  </ul>
+</nav>
+<section id="bg">
+  <section id="overlay"></section>
+  <a href="#" class="nextImageBtn" title="next"></a><a href="#" class="prevImageBtn" title="previous"></a><img src="images/gallery/1.jpg" alt="Orgullosamente San Martinense" title="Orgullosamente San Martinense" id="bgimg" /></section>
+<div id="preloader"><img src="images/ajax-loader_dark.gif" width="32" height="32" alt="" /></div>
+<div id="img_title"></div>
+<div id="toolbar"><a href="#" title="Maximize" onClick="ImageViewMode('full');return false"><img src="images/toolbar_fs_icon.png" width="50" height="50" alt="" /></a></div>
+<div id="thumbnails_wrapper">
+  <div id="outer_container">
+    <div class="thumbScroller">
+      <div class="container">
+        <div class="content">
+          <div><a href="images/gallery/1.jpg"><img src="images/gallery/1_thumb.jpg" title="Orgullosamente San Martinense" alt="Orgullosamente San Martinense" class="thumb" /></a></div>
+        </div>
+        <div class="content">
+          <div><a href="images/gallery/2.jpg"><img src="images/gallery/2_thumb.jpg" title="Flota de Última Generación" alt="Flota de Última Generación" class="thumb" /></a></div>
+        </div>
+        <!--<div class="content">
+          <div><a href="images/gallery/4.jpg"><img src="images/gallery/4_thumb.jpg" title="Personal Capacitado" alt="Personal Capacitado" class="thumb" /></a></div>
+        </div>-->
+        <div class="content">
+          <div><a href="images/gallery/5.jpg"><img src="images/gallery/5_thumb.jpg" title="Planta Envasadora de Calidad" alt="Planta Envasadora de Calidad" class="thumb" /></a></div>
+        </div>
+        <div class="content">
+          <div><a href="images/gallery/6.jpg"><img src="images/gallery/6_thumb.jpg" title="Entrega Asegurada a Domicilio y Productos de Gran Calidad" alt="Entrega Asegurada a Domicilio" class="thumb" /></a></div>
+        </div>
+		<div class="content">
+          <div><a href="images/gallery/1.jpg"><img src="images/gallery/1_thumb.jpg" title="Orgullosamente San Martinense" alt="Orgullosamente San Martinense" class="thumb" /></a></div>
+        </div>
+        <div class="content">
+          <div><a href="images/gallery/2.jpg"><img src="images/gallery/2_thumb.jpg" title="Flota de Última Generación" alt="Flota de Última Generación" class="thumb" /></a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="scripts/gallery.js"></script> 
+<script type="text/javascript" src="scripts/jquery.mCustomScrollbar.js"></script>
+</body>
+</html>
---
+++ unigas_frontend/index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html style="margin-top: 0 !important" class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html style="margin-top: 0 !important" class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html style="margin-top: 0 !important" class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <!--<![endif]-->
+<html style="margin-top: 0 !important" lang="en-US"> <!--<![endif]-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>..:: UNIGAS | Lo Nuestro Siempre Primero. ::..</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,300italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all" >
+    <link rel="stylesheet" href="assets/css/responsive.css" type="text/css" media="all" >
+	<link rel="shortcut icon" href="assets/img/icon.png" type="image/x-icon" />	
+   	<script src="assets/js/jquery.js"></script>
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+       <link href="assets/css/ie.css" rel='stylesheet' type='text/css'>
+    <![endif]-->
+    
+    <!--[if IE 7]>
+		<link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css">
+	<![endif]-->
+</head>
+<body data-spy="scroll" data-target=".navbar">
+<header id="header">
+	<div class="header-holder">
+		<div class="container hidden-phone">
+			<div class="brow">
+				<div class="brick1 logo_container">
+					<a href="http://unigas.herokuapp.com/" id="index" class="nav-item clearfix">
+						<div class="nav-hover"></div>
+						<img src="assets/img/logo_unigas.png">
+					</a>
+				</div>
+				<div class="brick1 transparent"></div>
+			</div>
+			<div class="brow">
+				<div class="slogan brick3 boffset1 transparent text-right">
+					<div class="inner">
+						<h1>Unigas</h1>
+					<p>Nuestra Empresa.</p>	
+					</div>
+					
+				</div>
+				<div class="brick1 odd">
+					<a href="#quienes" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_bulb"></i>
+						<span>Quienes Somos</span>
+					</a>
+				</div>
+				<div class="brick1 thumb">
+					<div class="nav-item flipX">
+						<img class="img1" src="assets/img/img-slide1.png" alt="">
+						<img class="img2" src="assets/img/img-slide7.png" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="brow">
+				<div class="brick1 thumb">
+					<div class="nav-item flipX">
+						<img class="img1" src="assets/img/img-slide2.png" alt="">
+						<img class="img2" src="assets/img/img-slide8.png" alt="">
+					</div>
+				</div>
+				<div class="brick1 odd">
+					<a href="#historia" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_photo"></i>
+						<span>Hisotria</span>
+					</a>
+				</div>
+				<div class="brick1">
+					<a href="#about-us" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_bubble"></i>
+						<span>Misión y Visión</span>
+					</a>
+				</div>
+				<div class="brick1 thumb">
+					<div class="nav-item flipY">
+						<img class="img1" src="assets/img/img-slide3.png" alt="">
+						<img class="img2" src="assets/img/img-slide9.png" alt="">
+					</div>
+				</div>
+				<div class="brick1 thumb">
+					<div class="nav-item flipX">
+						<img class="img1" src="assets/img/img-slide4.png" alt="">
+						<img class="img2" src="assets/img/img-slide10.png" alt="">
+					</div>
+				</div>
+				<div class="brick1">
+					<a href="#blog" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_note"></i>
+						<span>Responsabilidad Social</span>
+					</a>
+				</div>
+			</div>
+			<div class="brow">
+				<div class="brick1 boffset2 thumb">
+					<div class="nav-item flipY">
+						<img class="img1" src="assets/img/img-slide5.png" alt="">
+						<img class="img2" src="assets/img/img-slide11.png" alt="">
+					</div>
+				</div>
+				<div class="brick1 thumb">
+					<div class="nav-item flipY">
+						<img class="img1" src="assets/img/img-slide6.png" alt="">
+						<img class="img2" src="assets/img/img-slide12.png" alt="">
+					</div>
+				</div>
+				<div class="brick1 odd">
+					<a href="#contact" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_paperplane"></i>
+						<span>Responsabilidad Ambiental</span>
+					</a>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="container visible-phone">
+			<div class="brick1 logo_container">
+					<a href="http://unigas.herokuapp.com/" class="nav-item clearfix">
+						<div class="nav-hover"></div>
+						<img src="assets/img/logo_unigas.png">
+					</a>
+				</div>
+				<div class="brick1 odd">
+					<a href="#quienes" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_bulb"></i>
+						<span>Quienes Somos</span>
+					</a>
+				</div>
+				<div class="brick1 odd">
+					<a href="#historia" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_photo"></i>
+						<span>Historia</span>
+					</a>
+				</div>
+				<div class="brick1">
+					<a href="#about-us" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_bubble"></i>
+						<span>Visión y Misión</span>
+					</a>
+				</div>
+				<div class="brick1">
+					<a href="#blog" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_note"></i>
+						<span>Responsabilidad Social</span>
+					</a>
+				</div>
+				<div class="brick1 odd">
+					<a href="#contact" class="nav-item">
+						<div class="nav-hover"></div>
+						<i class="li_paperplane"></i>
+						<span>Responsabilidad Ambiental</span>
+					</a>
+				</div>
+		</div>
+	</div>
+</header> <!-- End header -->
+<div id="navbar" class="navbar">
+  <div class="navbar-inner">
+    <div class="container">
+      <button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar" type="button">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="#" class="brand">UNIGAS</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+          <li class="">
+            <a href="#quienes"><i class="li_bulb"></i>Quienes Somos</a>
+          </li>
+          <li class="">
+            <a href="#historia"><i class="li_photo"></i>Hist.</a>
+          </li>
+           <li class="">
+            <a href="#about-us"><i class="li_bubble"></i>Mis. y Vis.</a>
+          </li>
+          <li class="">
+            <a href="#blog"><i class="li_note"></i>Resp. Soc.</a>
+          </li>
+          <li class="">
+            <a href="#contact"><i class="li_paperplane"></i>Resp. Amb.</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="quienes" class="section">
+	<div class="container">
+		<div class="hero">
+			<h1>Quienes Somos</h1>
+		</div>
+		<div class="row">
+			<div class="span6" style="height: 320px !important;">
+				<p>Somos Universal Gas S.R.L., empresa Sanmartinense dedicada al envasado y comercialización de Gas Licuado de Petróleo desde el año 1997, cumpliendo con un labor importante, y que durante el tiempo de sus operaciones se ha venido consolidando como uno de los principales agentes en la venta del gas para la industria y los hogares de la región.</p>
+				<p>Nuestro trabajo está destinado a brindar la fuente de eneria necesaria para las familias, el sector comercial alimenticio y de producción. Es así que Universal gas S.R.L. mantiene su compromiso con sus usuarios de brindar permanentemente un producto que cumpla los más altos estándares de calidad y desarrollar las mejores prácticas de servicios.</p>
+			</div>
+			<div class="span6">
+				    <img src="assets/img/1.png">
+			</div>
+		</div>
+	</div>
+</div> <!-- end About us  -->
+<div id="historia" class="section">
+	<div class="container">
+		<div class="hero">
+			<h1>Historia</h1>
+		</div>
+		<div class="row">
+			<div class="span6">
+				    <img src="assets/img/1.png">
+			</div>
+			<div class="span6" style="height: 320px !important;">
+				<p>Universal Gas S.R.L. se constituye el 19 de Enero de 1996 e inicia su actividad comercial el 27 de setiembre de 1997, teniendo como inversionista principal al Sr. Walter Antonio Lozano Guerra.</p>
+				<p>La empresa instaló la planta de GLP como una de las primeras de la región en su local ubicado en la Av. Salaverry Cdra. 7 del distrito de Morales, el mismo que en el año 2003 por razones de caracter comercial se transfiere a terceros; para luego instalar la planta en su nuevo local de más de 5000 m2 ubicada en el Jr. Francisco Pizarro N° 860 del mismo distrito de Morales.</p>
+			</div>
+			
+		</div>
+	</div>
+</div> <!-- end About us  -->
+<footer id="footer">
+	<div class="container">
+		&copy; 2014 <a href="http://unigas.com.pe">UNIGAS | Jr. Francisco Pizarro #860 Morales. Fijo: 522727 - 526544 Rpm: *239153</a>. Todos los derechos reservados. Desarrollado por : <a href="http://hsctch.com">HSC Technology</a>
+	</div>
+	<a href="#" id="btn_up">ARRIBA</a>
+</footer>
+<!-- Bootstrap js -->
+<script src="assets/js/bootstrap-transition.js"></script>
+<script src="assets/js/bootstrap-modal.js"></script>
+<script src="assets/js/bootstrap-dropdown.js"></script>
+<script src="assets/js/bootstrap-carousel.js"></script>
+<script src="assets/js/bootstrap-collapse.js"></script>
+<script src="assets/js/bootstrap-scrollspy.js"></script>
+
+<script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="assets/js/jquery.mousewheel.min.js"></script>
+<script src="assets/js/jquery.hoverdir.js"></script>
+<script src="assets/js/jquery.isotope.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script>
+	$('#header a.nav-item').unbind('click');
+</script>
+
+</body>
+</html>
